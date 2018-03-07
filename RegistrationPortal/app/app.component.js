feb6a009267f2ba167527268a6d74583f07ffcbd
bises <acharya.bises@gmail.com>
2018-03-06 23:07:44 -0800

    updated the typecript version to 2.7.2

--- RegistrationPortal/app/app.component.js
+++ RegistrationPortal/app/app.component.js
@@ -7,17 +7,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var AppComponent = (function () {
+var AppComponent = /** @class */ (function () {
     function AppComponent() {
         this.name = 'Angular v 5';
     }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "<h1>Hello {{name}}</h1>",
+        })
+    ], AppComponent);
     return AppComponent;
 }());
-AppComponent = __decorate([
-    core_1.Component({
-        selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
-    })
-], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
