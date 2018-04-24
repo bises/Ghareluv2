86890e1899c51e05e7eb24a5f8ba96ce0956b6fd
bises <acharya.bises@gmail.com>
2018-04-23 21:31:53 -0700

    Added more models

---
+++ Database/Models/Udhyog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class Udhyog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("RegistrationNumber")]
+        public int RegNum { get; set; }
+
+        [Required]
+        [Column("RegistrationDate")]
+        public DateTimeOffset RegDate { get; set; }
+
+        [Required]
+        public int TypeId { get; set; }
+
+        [Required]
+        public int ClassId { get; set; }
+
+        [Required]
+        public int SubClassId { get; set; }
+
+        [Required]
+        [Column("CompanyName")]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int PanNo { get; set; }
+
+        public int AddressId { get; set; }
+
+        [ForeignKey("TypeId")]
+        public virtual UdhyogType Type { get; set; }
+
+        [ForeignKey("ClassId")]
+        public virtual UdhyogClass Class { get; set; }
+
+        [ForeignKey("SubClassId")]
+        public virtual UdhyogSubClass SubClass { get; set; }
+
+        [ForeignKey("AddressId")]
+        public virtual Address Address { get; set; }
+    }
+}
